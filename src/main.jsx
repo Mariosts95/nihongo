9a75afb1dd7f130c72ff58b9687e66f5bca2bca5
Marios Tsioutsis <marios.tsioutsis1@gmail.com>
2023-04-08 01:30:42 +0300

    Initialize google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ReactGA from 'react-ga4';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import GlobalProvider from '@/store/GlobalProvider';
 
 import App from '@/App.jsx';
+
+ReactGA.initialize('G-C4JNGV8HTM');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
